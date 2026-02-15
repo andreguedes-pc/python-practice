21ee78553e3daf4f9a394ccb966cd03a6930fb50
andreguedes-pc <andresguedes@gmail.com>
2026-02-15 10:31:07 -0500

    Add files via upload

---
+++ name_cases.py
@@ -0,0 +1,8 @@
+# Ask user for his name
+first_name = input("What is your name?")
+print("Thank you!")
+# Ask user for his last name
+last_name = input("Now, what is your last name?")
+#Organize the full name
+full_name = f"{first_name} {last_name}"
+print(f"Howdy!,\n{full_name.title()}")
