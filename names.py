e961805d604560809595b6cd878ba771aca7c270
andreguedes-pc <andresguedes@gmail.com>
2026-02-20 17:19:33 -0500

    Add files via upload

---
+++ names.py
@@ -0,0 +1,7 @@
+names = ["John","Kate","Megan","Julia"]
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
+message = f"It is very nice to have a close friend as {names[0].capitalize()}"
+print(message)
